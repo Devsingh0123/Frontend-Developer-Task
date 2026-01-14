9f6f94aa866c247ffe6dbe783e548af121d6a58b
Devsingh0123 <dayamns20@gmail.com>
2026-01-15 01:20:39 +0530

    backend deployed url added on frontend side

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,10 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+      "http://localhost:5173"
+      
+    ],
   credentials: true,
 }));
 app.use(express.json());
