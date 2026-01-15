e2392353ad7bcf94fcff926634e538bef8a00892
Devsingh0123 <dayamns20@gmail.com>
2026-01-15 10:33:12 +0530

    Hashrouter added insted of browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 import { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchCurrentUser } from "./redux/authSlice/authSlice";
 import AppRoutes from "./routes/AppRoutes";
@@ -16,10 +15,13 @@ function App() {
 
   return (
     <>
-  <Toaster position="top-center" reverseOrder={false} />
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter></>
+      <Toaster position="top-center" reverseOrder={false} />
+      {/* <BrowserRouter> */}
+      <HashRouter>
+        <AppRoutes />
+      </HashRouter>
+      {/* </BrowserRouter></> */}
+    </>
   );
 }
 
